a78933ef990195c145e57711c5fbc9c1fcad48e1
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-04-20 22:12:26 -0500

    finish lesson 1

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -1,44 +1,42 @@
 import { reducer } from '../utils/reducers';
-// import actions
 import {
-    UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY,
-    UPDATE_PRODUCTS
-} from '../utils/actions'
+  UPDATE_PRODUCTS,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY
+} from '../utils/actions';
 
-// create sample of what global state will look like
 const initialState = {
-    products: [],
-    categories: [{ name: 'Food'}],
-    currentCategory: '1'
-}
+  products: [],
+  categories: [{ name: 'Food' }],
+  currentCategory: '1',
+};
 
 test('UPDATE_PRODUCTS', () => {
-    let newState = reducer(initialState, {
-      type: UPDATE_PRODUCTS,
-      products: [{}, {}]
-    });
-  
-    expect(newState.products.length).toBe(2);
-    expect(initialState.products.length).toBe(0);
+  let newState = reducer(initialState, {
+    type: UPDATE_PRODUCTS,
+    products: [{}, {}]
   });
+
+  expect(newState.products.length).toBe(2);
+  expect(initialState.products.length).toBe(0);
+});
 
 test('UPDATE_CATEGORIES', () => {
-    let newState = reducer(initialState, {
-        type:UPDATE_CATEGORIES,
-        categories:[{}, {}]
-    })
+  let newState = reducer(initialState, {
+    type: UPDATE_CATEGORIES,
+    categories: [{}, {}]
+  });
 
-    expect(newState.categories.length).toBe(2)
-    expect(initialState.categories.length).toBe(1)
-})
+  expect(newState.categories.length).toBe(2);
+  expect(initialState.categories.length).toBe(1);
+});
 
 test('UPDATE_CURRENT_CATEGORY', () => {
-    let newState = reducer(initialState, {
-      type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: '2'
-    });
-  
-    expect(newState.currentCategory).toBe('2');
-    expect(initialState.currentCategory).toBe('1');
+  let newState = reducer(initialState, {
+    type: UPDATE_CURRENT_CATEGORY,
+    currentCategory: '2'
   });
+
+  expect(newState.currentCategory).toBe('2');
+  expect(initialState.currentCategory).toBe('1');
+});
