5d4a23adfd5989d58d593bd4198c57c845e02982
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-04-20 22:17:12 -0500

    finish lesson 1

---
+++ client/build/precache-manifest.244cdd3bb7cc9f9bc00c88ff21301479.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187013eebf4d84bbec3d0dbecbb1b9e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c3d921165d96071f2ee",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "130c744298700e5891bb",
+    "url": "/static/js/2.03f92a88.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.03f92a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c3d921165d96071f2ee",
+    "url": "/static/js/main.6b8bea0c.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
